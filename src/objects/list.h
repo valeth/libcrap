1b7e8884aa7f0048f00c233001f74607c374fdd5
Patrick Auernig <patrick.auernig@mykolab.com>
2018-04-26 19:51:58 +0200

    Add documentation for list

--- src/objects/list.h
+++ src/objects/list.h
@@ -16,45 +16,104 @@ struct _DListNode;
 
 struct _DList;
 
+/// A single list node
 typedef struct _DListNode* ListNode;
 
+/// A doubly linked list
 typedef struct _DList* List;
 
+/// A function that can be used with `list_map`.
 typedef Value (*FnListMap)(ListNode);
 
+/// A function that can be used with `list_each`.
 typedef void (*FnListEach)(ListNode);
 
 
+/// Creates a new empty list.
+/// @retval  List, nil
 List list_new();
 
-void list_drop(List);
+/// Destroys a list object.
+/// @param  list  The list to destroy
+/// @attention  Stored values will not be `free`'d
+void list_drop(List list);
 
+/// Removes elements from a list
+/// @warning  This will `free` elements, do not use with non-`malloc`'d content.
 void list_clear(List);
 
-void list_insert(List, Index, Value);
+/// Inserts a value into a list.
+/// @param  list  The list to insert into
+/// @param  idx  The position to insert at
+/// @param  val  The value to insert
+void list_insert(List list, Index idx, Value val);
 
-void list_append(List, Value);
+/// Inserts a value at the end of the list.
+/// @param  list  The list to insert into
+/// @param  val  The value to insert
+void list_append(List list, Value val);
 
-void list_prepend(List, Value);
+/// Inserts a value at the beginning of the list.
+/// @param  list  The list to insert into
+/// @param  val  The value to insert
+void list_prepend(List list, Value val);
 
-Value list_delete(List, Index);
+/// Deletes a value from a list.
+/// @param  list  The list to delete from
+/// @param  idx  The position to delete
+/// @retval  Value
+Value list_delete(List list, Index idx);
 
-Value list_shift(List);
+/// Deletes a value from the beginning of the list.
+/// @param  list  The list to delete from
+/// @retval  Value
+Value list_shift(List list);
 
-Value list_pop(List);
+/// Deletes a value from the end of the list.
+/// @param  list  The list to delete from
+/// @retval  Value
+Value list_pop(List list);
 
-Value list_get(List, Index);
+/// Fetches a value at the given position.
+/// @param  list  The list to query
+/// @param  idx  The position to get
+/// @retval  Value
+Value list_get(List list, Index idx);
 
-Value list_first(List);
+/// Fetches the first value in the list.
+/// @param  list  The list to query
+/// @retval  Value
+Value list_first(List list);
 
-Value list_last(List);
+/// Fetches the last value in the list.
+/// @param  list  The list to query
+/// @retval  Value
+Value list_last(List list);
 
-bool list_is_empty(List);
+/// Checks if the list is empty.
+/// @param  list
+/// @retval  bool
+bool list_is_empty(List list);
 
-Size list_size(List);
+/// Gets the current list size.
+/// @param  list
+/// @retval  Size
+Size list_size(List list);
 
-List list_extend(List, List);
+/// Extends a list with the contents of another one.
+/// @param  list  The list to extend
+/// @param  other  The list that gets copied
+/// @retval  List  A pointer to `list`
+List list_extend(List list, List other);
 
-void list_each(List, FnListEach);
+/// Iterate over each element in the list and apply a function to it.
+/// @param  list  The list to iterate over
+/// @param  func  The function to apply to every node
+void list_each(List list, FnListEach func);
 
+/// Iterate over each element in the list, apply a function to it, and return a modified list.
+/// This will not modify the current list.
+/// @param  list  The list to iterate over
+/// @param  func  The function to apply to every node
+/// @retval  List  The modified list
 List list_map(List, FnListMap);
